edb564426fe92461ece59bb664f6add077d21d34
devpupo <devpupo@outlook.com>
2025-09-09 15:38:51 -0300

    fix: audit migration

---
+++ prisma/migrations/20250909183800_add_audit_fields/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "public"."Address" ADD COLUMN     "createdBy" TEXT,
+ADD COLUMN     "updatedBy" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Customer" ADD COLUMN     "createdBy" TEXT,
+ADD COLUMN     "updatedBy" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Dependent" ADD COLUMN     "createdBy" TEXT,
+ADD COLUMN     "updatedBy" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Document" ADD COLUMN     "createdBy" TEXT,
+ADD COLUMN     "updatedBy" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."User" ADD COLUMN     "createdBy" TEXT,
+ADD COLUMN     "updatedBy" TEXT;
