18d8bd25255635be18cb3d7b69f593c6057dfe0b
devpupo <devpupo@outlook.com>
2025-09-19 09:40:09 -0300

    improv: create user role

--- src/application/use-cases/user/create-user.ts
+++ src/application/use-cases/user/create-user.ts
@@ -1,13 +1,17 @@
 import { CreateUserResponse } from "@/application/dto/create-user.dto";
 import { IUserRepository } from "@/domain/repositories/user.repository";
 import { AuditContext } from "@/application/dto/audit-context.dto";
-import { User } from "@prisma/client";
+import { User, UserRole } from "@prisma/client";
+import HttpError from "@/shared/errors/http.error";
 
 export class CreateUserUseCase {
   constructor(private readonly userRepository: IUserRepository) { }
 
   async execute(user: User, auditContext?: AuditContext): Promise<CreateUserResponse> {
-    console.log(JSON.stringify(user))
+    if (user.role !== UserRole.CUSTOMER) {
+      throw new HttpError(400, "Only users with CUSTOMER role can be created");
+    }
+
     const data = await this.userRepository.save(user, auditContext);
 
     return {
